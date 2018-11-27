d55f9ebde583f5bbde309bc1d7bcb3c306f35e22
Victoria Juan <vjuan@mit.edu>
2018-11-27 15:17:34 -0500

    Toggle raw string

--- data/drafting.php
+++ data/drafting.php
@@ -6,12 +6,20 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
+// get parameter values; must specify date and measurement
 $date = isset($_GET['date']) ? $_GET['date'] : die();
 $measure = isset($_GET['measure']) ? $_GET['measure'] : die();
-$raw = isset($_GET['raw']) ? $_GET['raw'] : die();
+$raw = isset($_GET['raw']) ? $_GET['raw'] : FALSE;
+$rawstr = "";
+// set raw
+if ((bool)$raw) {
+  $rawstr = "raw/";
+}
 
-$addr = "../../../../../home/pi/Documents/testing/sense_hat/data/nov15/" . $measure . "-" . $date . ".csv";
-// check request filed exists
+// create address
+$addr = "../../../../../home/pi/Documents/testing/sense_hat/data/nov15/" . $rawstr . $measure . "-" . $date . ".csv";
+
+// check requested file exists
 if(file_exists($addr)){
   //Open our CSV file using the fopen function.
   $fh = fopen($addr, "r");
@@ -26,14 +34,11 @@ if(file_exists($addr)){
 
   // set response code - 200 OK
   http_response_code(200);
-
   // show products data in json format
   echo json_encode($csvData);
 } else{
-
     // set response code - 404 Not found
     http_response_code(404);
-
     // tell the user no products found
     echo json_encode(
         array("message" => "No data found at " . $addr)
